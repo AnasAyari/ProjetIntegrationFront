f19ad379522f40ec0f9f47361cc929540d26e1b4
MeDjb10 <131858818+MeDjb10@users.noreply.github.com>
2024-05-16 21:11:44 +0100

    Fixed all the url paths Add multiple roles support in user registration form

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,97 +10,86 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
-
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[Route('/user')]
 class UserController extends AbstractController
 {
     #[Route('/Users', name: 'app_user_index', methods: ['GET'])]
-    public function index(UserRepository $userRepository, HttpClientInterface $client,SerializerInterface $serializer): Response
+    public function index(UserRepository $userRepository, SerializerInterface $serializer): Response
     {
-        $data = $userRepository->findAll();
-        //$users = $response->toArray();
-        // $uD= new User($userData['id'],$userData['username'],UserData['email'],$userData['password'],);
-        // return $this->render(
-        //     'user/index.html.twig',
-        //     [
-        //     'users' => $userRepository->findAll(),
-        //     ]
-        // );
-        $response = $serializer->serialize($data, 'json');
-        return new JsonResponse($response, 200, [], true);
-        
+        try {
+            $data = $userRepository->findAll();
+            $response = $serializer->serialize($data, 'json');
+            return new JsonResponse($response, 200, [], true);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
-    #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'app_user_new', methods: ['POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
+    try {
         $user = new User();
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
+        $data = json_decode($request->getContent(), true);
+        $user->setUsername($data['username']);
+        $user->setEmail($data['email']);
+        $user->setPassword($data['password']);
+        $user->setNumero($data['numero']);
+        $entityManager->persist($user);
+        $entityManager->flush();
+        return new JsonResponse(['status' => 'User created'], Response::HTTP_CREATED);
+    } catch (\Exception $e) {
+        return new JsonResponse(['error' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
+}
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+#[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
+public function show($id, UserRepository $userRepository, SerializerInterface $serializer): Response
+{
+    try {
+        $user = $userRepository->find($id);
+        if ($user === null) {
+            throw new \Exception('User not found');
         }
-
-        return $this->renderForm('user/new.html.twig', [
-            'user' => $user,
-            'form' => $form,
-        ]);
+        $response = $serializer->serialize($user, 'json');
+        return new JsonResponse($response, 200, [], true);
+    } catch (\Exception $e) {
+        return new JsonResponse(['error' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
     }
+}
 
-    #[Route('/user/{id}', name: 'app_user_show', methods: ['GET'])]
-    public function show(User $user): Response
-    {
-        return $this->render('user/show.html.twig', [
-            'user' => $user,
-        ]);
+#[Route('/{id}/edit', name: 'app_user_edit', methods: ['PUT'])]
+public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+{
+    try {
+        $data = json_decode($request->getContent(), true);
+        $user->setUsername($data['username']);
+        $user->setEmail($data['email']);
+        $user->setPassword($data['password']);
+        $user->setNumero($data['numero']);
+        $entityManager->flush();
+        return new JsonResponse(['status' => 'User updated'], Response::HTTP_OK);
+    } catch (\Exception $e) {
+        return new JsonResponse(['error' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
     }
+}
 
-    #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
-    {
-        $uD= new User($userData['id'],$userData['username'],UserData['email'],$userData['password'],);
-        return $this->render(
-            'Users/index.html.twig',
-            ['user' => $uD,]
-            // array('users' => $users,)
-            //[
-            // 'users' => $userRepository->findAll(),
-
-            //]
-        );
-
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+#[Route('/{id}', name: 'app_user_delete', methods: ['DELETE'])]
+public function delete($id, UserRepository $userRepository, EntityManagerInterface $entityManager): Response
+{
+    try {
+        $user = $userRepository->find($id);
+        if ($user === null) {
+            throw new \Exception('User not found');
         }
-
-        return $this->renderForm('user/edit.html.twig', [
-            'user' => $user,
-            'form' => $form,
-        ]);
+        $entityManager->remove($user);
+        $entityManager->flush();
+        return new JsonResponse(['status' => 'User deleted'], Response::HTTP_NO_CONTENT);
+    } catch (\Exception $e) {
+        return new JsonResponse(['error' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
     }
-
-    #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
-    public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('_token'))) {
-            $entityManager->remove($user);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
-    }
+}
 }
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class UserType extends AbstractType
 {
@@ -13,7 +14,15 @@ class UserType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('roles')
+            ->add('roles', ChoiceType::class, [
+        'choices' => [
+            'User' => 'ROLE_USER',
+            'Admin' => 'ROLE_ADMIN',
+            // add more roles if needed
+        ],
+        'multiple' => true,
+        'expanded' => true,
+    ])
             ->add('password')
             ->add('numero')
             ->add('username')
